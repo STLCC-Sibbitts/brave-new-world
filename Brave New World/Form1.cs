7b0fcf12aafed49556618a04fb05917307a43e97
4tutoralcom <4tutoralcom@gmail.com>
2014-04-10 00:34:44 -0500

    Beginning Land Classes for random Generation

--- Brave New World/Form1.cs
+++ Brave New World/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Windows;
 namespace Brave_New_World
 {
     public partial class Form1 : Form
@@ -14,6 +14,23 @@ namespace Brave_New_World
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const int MAXSECTIONS = 24;
+            const int MAXLENGTH = 10;
+            //random value
+            Random randNumGen = new Random();
+            List<List<List<Vector>>> mapdata = new List<List<List<Vector>>>();
+
+            mapdata.Add(new List<List<Vector>>());
+            mapdata[0].Add(new List<Vector>());
+            double theata = randNumGen.Next(1, MAXSECTIONS);
+            int r = randNumGen.Next(1, MAXLENGTH);
+            mapdata[0][0].Add(new Vector(Math.Cos(Math.PI * theata / (MAXSECTIONS / 2)) * r, Math.Sin(Math.PI * theata / (MAXSECTIONS / 2))) * r);
+            Vector d = mapdata[0][0][0];
+            MessageBox.Show(String.Format("[{3},{4}] | [{0},{1}], length of {2}", d.X, d.Y, d.Length, theata * 360 / MAXSECTIONS,r));
         }
     }
 }
---
+++ VectorLandMesh/LandMesh.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using VectorLandMesh;
+
+namespace vectorTest
+{
+    /// <summary>
+    /// Land Mesh Class 
+    /// </summary>
+    class LandMesh
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<LandContour> LandVectorData{ get; private set;}
+        /// <summary>
+        /// 
+        /// </summary>
+        public Point CenterPoint { get; set; }
+
+        /// <summary>
+        /// Genarate New Land Mesh Based on a Random Object
+        /// </summary>
+        public LandMesh(Random seed)
+        {
+            const int maxX = 16, maxY = 16;
+            if (seed!=null)
+            {
+                this.CenterPoint = new Point(seed.Next(maxX), seed.Next(maxY));
+            }
+        }
+       
+        public LandMesh(Double CenterX = 0, Double CenterY = 0, LandContour contour=null)
+        {
+            this.CenterPoint = new Point(CenterX, CenterY);
+            this.LandVectorData = new List<LandContour>();
+            if(contour!=null)
+                this.LandVectorData.Add(contour);
+        }
+    }
+}
